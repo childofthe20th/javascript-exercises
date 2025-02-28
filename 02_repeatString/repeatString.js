5a5235b95f7d84ea8c372edbb57facaa47dd8861
Eric Sanchez <bluemojo82@gmail.com>
2025-02-28 01:19:01 -0500

    fixed vscode flag

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -10,13 +10,5 @@ const repeatString = function(string, num) {
     }
 };
 
-repeatString('hey', 3); // heyheyhey
-repeatString('hello', 10);
-repeatString('hi', 1);
-repeatString('bye', 0);
-repeatString('goodbye', -1);
-repeatString('hey', 10);
-repeatString('', 10);
-
 // Do not edit below this line
 module.exports = repeatString;
