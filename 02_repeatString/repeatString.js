e2b4a8a79eaa8e358be27d7f3c7488faee9dcc69
Eric Sanchez <bluemojo82@gmail.com>
2025-02-28 01:13:50 -0500

    completed tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,22 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let result = "";
+    for (let i = 0; i < num; i++) {
+        result += string;
+    }
+    if (num < 0) {
+        return 'ERROR';
+    } else {
+        return result;
+    }
 };
+
+repeatString('hey', 3); // heyheyhey
+repeatString('hello', 10);
+repeatString('hi', 1);
+repeatString('bye', 0);
+repeatString('goodbye', -1);
+repeatString('hey', 10);
+repeatString('', 10);
 
 // Do not edit below this line
 module.exports = repeatString;
