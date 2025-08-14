b41362737943a58acf9b6132dd8e6fe2b8aad486
Eric Sanchez <bluemojo82@gmail.com>
2025-08-14 00:08:27 -0400

    all tests passed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,31 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    // Convert input to a number if it's a string
+    if (typeof num === 'string') {
+        num = parseInt(num, 10);
+    }
+    
+    // Handle negative numbers
+    if (num < 0) {
+        return "OOPS";
+    }
+    
+    // Base cases
+    if (num === 0) return 0;
+    if (num === 1) return 1;
+    
+    // Initialize the first two Fibonacci numbers
+    let a = 0, b = 1, temp;
+    
+    // Calculate Fibonacci number iteratively
+    for (let i = 2; i <= num; i++) {
+        temp = a + b;
+        a = b;
+        b = temp;
+    }
+    
+    return b;
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
