40ab6993f835de38817bf94409a9377c2eab26b6
Eric Sanchez <bluemojo82@gmail.com>
2025-08-14 00:05:15 -0400

    all tests passed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const cleaned = str.replace(/[^a-z0-9]/gi, '').toLowerCase();
+    const reversed = cleaned.split('').reverse().join('');
+    return cleaned === reversed;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
